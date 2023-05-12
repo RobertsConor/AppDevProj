bb263599fec412cd51b7ede62cf61cf6af499434
Conor Roberts <conorrroberts@gmail.com>
2023-05-12 01:54:32 +0100

    updated

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Browser } from '@capacitor/browser' // import Browser from Capacitor Browser plugin
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-stories:any[]=[];
-  constructor() {}
-  
+
+  constructor(private navCtrl: NavController) {}
+
+  // navigate to the UpcomingPage
+  goToUpcomingPage() {
+    this.navCtrl.navigateForward('/upcoming');
+  }
+
+  // navigate to the FavouritesPage
+  goToFavouritesPage() {
+    this.navCtrl.navigateForward('/favourites')
+  }
+
+  // navigate to the TrendingPage
+  goToTrendingPage() {
+    this.navCtrl.navigateForward('/trending')
+  }
+
+  // open the browser to go to the Cinema page
+  goToThisYearPage() {
+    this.openBrowser();
+  }
+
+  // open the browser using the Capacitor Browser plugin
+  async openBrowser() {
+    await Browser.open({ url: 'https://www.omniplex.ie/cinema/galway'});
+  }
 }
